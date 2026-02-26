06756f97fd629ce8b2029f7514389dc937af2f07
Valentin Regnault <valentinregnault22@gmail.com>
2026-02-26 15:33:07 +0100

    refactor: factorizing sleep function and removing unsused functions in gui

--- src/lib/concrete_ono_gui.ml
+++ src/lib/concrete_ono_gui.ml
@@ -2,22 +2,6 @@ let char_width = 30
 let row = ref 1
 let column = ref 1
 
-let gui_print_i32 (n : Kdo.Concrete.I32.t) : (unit, Owi.Result.err) Result.t =
-  Logs.app (fun m -> m "%a" Kdo.Concrete.I32.pp n);
-  Ok ()
-
-let gui_print_i64 (n : Kdo.Concrete.I64.t) : (unit, Owi.Result.err) Result.t =
-  Logs.app (fun m -> m "%a" Kdo.Concrete.I64.pp n);
-  Ok ()
-
-let gui_read_int () : (Kdo.Concrete.I32.t, Owi.Result.err) Result.t =
-  (* TODO: setup un readEntier pour le Gui *)
-  Ok (Kdo.Concrete.I32.of_int32 0l)
-
-let gui_sleep (seconds : Kdo.Concrete.I32.t) : (unit, Owi.Result.err) Result.t =
-  Unix.sleepf (float_of_int (Kdo.Concrete.I32.to_int seconds) /. 1000.0);
-  Ok ()
-
 let gui_newline () : (unit, Owi.Result.err) Result.t =
   column := 1;
   row := !row + 1;
@@ -41,24 +25,12 @@ let gui_print_cell (cell : Kdo.Concrete.I32.t) : (unit, Owi.Result.err) Result.t
   end;
   Ok ()
 
-let guiSet =
-  let open Concrete_ono_types in
-  {
-    print_i32 = gui_print_i32;
-    print_i64 = gui_print_i64;
-    read_int = gui_read_int;
-  }
-
 let functions =
   let open Kdo.Concrete.Extern_func in
   let open Kdo.Concrete.Extern_func.Syntax in
   begin
     Raylib.init_window 2120 1080 "raylib [core] example - basic window";
     [
-      ("print_i32", Extern_func (i32 ^->. unit, guiSet.print_i32));
-      ("print_i64", Extern_func (i64 ^->. unit, guiSet.print_i64));
-      ("read_int", Extern_func (unit ^->. i32, guiSet.read_int));
-      ("sleep", Extern_func (i32 ^->. unit, gui_sleep));
       ("newline", Extern_func (unit ^->. unit, gui_newline));
       ("clear_screen", Extern_func (unit ^->. unit, gui_clear_screen));
       ("print_cell", Extern_func (i32 ^->. unit, gui_print_cell));
--- src/lib/concrete_ono_module.ml
+++ src/lib/concrete_ono_module.ml
@@ -2,6 +2,10 @@ let random_i32 () : (Kdo.Concrete.I32.t, Owi.Result.err) Result.t =
   let n = Random.int32 Int32.max_int in
   Ok (Kdo.Concrete.I32.of_int32 n)
 
+let sleep (duration : Kdo.Concrete.I32.t) : (unit, Owi.Result.err) Result.t =
+  Unix.sleepf (float_of_int (Kdo.Concrete.I32.to_int duration) /. 1000.0);
+  Ok ()
+
 let m (use_graphical_window : bool) (steps : int) (display_last : int) =
   let casted_steps = Int32.of_int steps in
   let casted_display_last = Int32.of_int display_last in
@@ -18,6 +22,10 @@ let m (use_graphical_window : bool) (steps : int) (display_last : int) =
         Extern_func
           ( unit ^->. i32,
             fun () -> Ok (Kdo.Concrete.I32.of_int32 casted_display_last) ) );
+      ( "sleep",
+        Extern_func
+          (i32 ^->. unit, fun (duration : Kdo.Concrete.I32.t) -> sleep duration)
+      );
     ]
   in
   let functions =
