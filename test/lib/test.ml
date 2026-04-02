d48813acc04a21b859994d829f5f92ec10212b9e
Archie Beales <archiebeales@gmail.com>
2026-04-02 14:30:11 +0200

    test: random tests now work

--- test/lib/test.ml
+++ test/lib/test.ml
@@ -24,31 +24,30 @@ let run_concrete_read_int_with_input input =
   with_stdin_from_string input (fun () ->
       Concrete_driver.run ~source_file:(Fpath.v read_int_fixture) false 5 5)
 
-let test_read_int_accepts_valid_integer =
-  Test.make ~count:1 ~name:"read_int accepts valid integer input" unit
-    (fun () ->
-      match run_concrete_read_int_with_input "123\n" with
-      | Ok () -> true
-      | Error _ -> false)
-
-let test_read_int_fallbacks_on_invalid_integer =
-  Test.make ~count:1 ~name:"read_int falls back to 0 on invalid input" unit
-    (fun () ->
-      match run_concrete_read_int_with_input "not-an-int\n" with
-      | Ok () -> true
-      | Error _ -> false)
-
 let test_read_int_fallbacks_on_eof =
   Test.make ~count:1 ~name:"read_int falls back to 0 on eof" unit (fun () ->
       match run_concrete_read_int_with_input "" with
       | Ok () -> true
       | Error _ -> false)
 
+let is_decimal_int_string s =
+  let s1 = int_of_string_opt s in
+  match s1 with None -> false | _ -> true
+
 let () =
   let suite =
     [
-      test_read_int_accepts_valid_integer;
-      test_read_int_fallbacks_on_invalid_integer;
+      Test.make ~count:100 ~name:"read_int accepts valid integer input"
+        (make ~print:string_of_int Gen.int) (fun n ->
+          match run_concrete_read_int_with_input (string_of_int n ^ "\n") with
+          | Ok () -> true
+          | Error _ -> false);
+      Test.make ~count:100 ~name:"read_int falls back to invalid integer input"
+        (make ~print:Fun.id Gen.string) (fun s ->
+          QCheck.assume (not (is_decimal_int_string s));
+          match run_concrete_read_int_with_input (s ^ "\n") with
+          | Ok () -> true
+          | Error _ -> false);
       test_read_int_fallbacks_on_eof;
     ]
   in
